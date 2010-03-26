79c3428f9aca71ed01654765975c2fc195e63f59
twist <bellee@webit.de>
2010-03-26 16:24:50 +0100

    create_array_of_length         configvars

--- gitart.pl
+++ gitart.pl
@@ -2,7 +2,23 @@
 
 $hash = <STDIN>;
 
-print hex2bin($hash)."\n";
+$bin_string =  hex2bin_with_spaces($hash);
+
+@steps = create_steps($bin_string) ;
+my $x = 17;
+my $y = 9;
+my $offset_up = $x;
+my $offset_down = ($x * -1);
+my $offset_left = -1;
+my $offset_right = 1;
+
+@field = create_array_of_lenght($x*$y);
+
+
+
+foreach $step (@steps)
+{
+}
 
 
 
@@ -11,8 +27,7 @@ print hex2bin($hash)."\n";
 
 
 
-
-sub hex2bin
+sub hex2bin_with_spaces
 {
         my ($hex_string) = @_;
         my @hex_characters = split(//, $hex_string);
@@ -22,8 +37,41 @@ sub hex2bin
                 my $int = oct("0x".$char);
                 my $str = unpack("B32", pack("N", $int));
                 $str =~ /(....)$/;   # otherwise you'll get leading zeros
-                $bin_string.=($1." ");
+
+                $bin_string.=($1." "); 
         }
         return $bin_string;
+}
+
+sub create_steps
+{
+      my ($string) = @_; 
+
+      # example of how we want to traverse the binary string
+      # each step consists of two bits
+      # 1011 1111 0010 1101 1001 1111 <- the string, divided by spaces
+      #  2 1  4 3 ........            <- the  order
+
+      my @halfbytes = split(/ /, $string);
+      my  @steps;
+      foreach $halfbyte (@halfbytes)
+      {
+              $halfbyte =~ /(..)(..)/;
+              push (@steps, ($2, $1));
+      }
+      return @steps;
+}
+
+sub create_array_of_lenght
+{
+        my ($length) = @_;
+        @array;
+        for($i = 0; $i < $length; $i++)
+        {
+                my $int = 0;
+                push(@array, $int);
+        }
+        return @array
+        
 }
 
