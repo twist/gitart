87ded2f457d2975411a6c635b86fd04d89527069
ben <twist@anne.site>
2010-03-31 00:47:19 +0200

    licence

--- src/gitart.pl
+++ src/gitart.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/perl
 
-#
 #	gitart.pl
 #
-#	bellee@webit.de
-#	2010
-#
-#	
 #	creates an ascii art image from a supplied hexadecimal hash.
 #
 #
 #
+#
+#
+#	Copyright (C) 2010 Benjamin Bellee 
+#
+#	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#	as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+#	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#	See the GNU General Public License for more details.
 
 
 
